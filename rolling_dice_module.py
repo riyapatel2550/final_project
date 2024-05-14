2486864aa777a90e20c0e944078a6e546bcc2048
Riya Patel <rpatel05@terpmail.umd.edu>
2024-05-14 16:35:35 -0400

    Controlled how many dice rolled (default of 3) with an argument & added a test-case fulfilling the "5-step" process (2.2 requirement)

--- rolling_dice_module.py
+++ rolling_dice_module.py
@@ -1,9 +1,9 @@
 # rolling_dice_module
 import random
 
-def rolling_dice():
+def rolling_dice(dice_number =3):
     """Roll three dice and return the result."""
-    return [random.randint(1, 6) for _ in range(3)]
+    return [random.randint(1, 6) for _ in range(dice_number)]
 
 def score(dice):
     """Calculate the score based on the rolled dice."""
@@ -11,3 +11,21 @@ def score(dice):
         return 0
     else:
         return sum(dice)
+    
+def test_rolling_dice():
+    """Test rolling_dice()"""
+    print("Test rolling_dice():")
+    for _ in range(5):
+        dice_number = random.randint(1, 6)
+        print(f"Number of dice: {dice_number}, Roll result: {rolling_dice(dice_number)}")
+
+def test_score():
+    """Test score()"""
+    print("\nTest score():")
+    test_scores = [[1, 1, 1], [1, 2, 3], [4, 4, 4], [5, 5, 6], [2, 3, 3]]
+    for dice in test_scores:
+        print("dice:", dice, "score:", score(dice))
+
+if __name__ == "__main__":
+    test_rolling_dice()
+    test_score()
